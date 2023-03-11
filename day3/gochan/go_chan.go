2f1a91c649d25424357a0eab41c509393cd7e672
lazyspell <jeremy2975@yahoo.com>
2023-03-11 00:17:24 -0600

    more work for day 2

--- day3/gochan/go_chan.go
+++ day3/gochan/go_chan.go
@@ -17,4 +17,41 @@ func GoChan() {
 	}
 
 	time.Sleep(10 * time.Millisecond)
+
+	ch := make(chan string)
+	go func() {
+		ch <- "hi" // send
+	}()
+	msg := <-ch //recieve
+	fmt.Println(msg)
+
+	go func() {
+		for i := 0; i < 3; i++ {
+			msg := fmt.Sprintf("message #%d", i+1)
+			ch <- msg
+		}
+		close(ch)
+	}()
+	for msg := range ch {
+		fmt.Println("got: ", msg)
+	}
+	msg = <-ch // ch is closed
+	fmt.Printf("closed: %#v\n", msg)
 }
+
+/*
+For every value "n" in values, spin a goroutine that will
+- sleep "n" milliseconds
+- Send "n" over a channel
+*/
+func sleepSort(values []int) []int {
+
+}
+
+/* Channel semantics
+- send & receive will block until opposite operation (*)
+- receive from a closed channel will return a zero value without blocking
+- send to a closed channel will panic
+- closing a closed channel will panic
+- send/receive to a nil channel will block forever
+*/
